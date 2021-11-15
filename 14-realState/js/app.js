c41975daeadb09823deac235552795deb2337241
fabian <fabian.ortiz.hotmail.com>
2021-11-15 03:17:22 -0500

    finalizado carolinaspan

---
+++ 14-realState/js/app.js
@@ -0,0 +1,16 @@
+
+
+const imagenes = document.querySelectorAll('.propiedad__imagen');
+
+
+
+
+window.addEventListener('scroll',() => {
+
+    const scroll = this.scrollY / -20;
+
+    imagenes.forEach((imagen) => {
+        imagen.style.backgroundPositionY = `${scroll}px`;
+
+    })
+})
---
+++ 16-carolinaspan/js/app.js
@@ -0,0 +1,27 @@
+
+// window.swiper = new Swiper({
+//     el: '.slider__contenedor',
+//     slideClass: 'slider__slide',
+//     createElements: true,
+//     autoplay:{
+//         deplay: 5000
+//     },
+//     loop: true,
+//     pagination:true,
+//     navigation:true
+
+// });
+
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
